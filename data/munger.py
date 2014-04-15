e6e9bb153f2ded3fa826547e07642a491cada932
Jarrod Millman <jarrod.millman@gmail.com>
2014-04-14 18:37:54 -0700

    clean up

--- data/munger.py
+++ data/munger.py
@@ -2,13 +2,10 @@ import csv
 import os  
 import numpy as np
 
-#fname = "raw/unc.edu.2d67443d-14ec-48f7-93e0-e409b1729587.1201146.rsem.genes.results"
-
 def get_path(name, base=os.path.dirname(__file__)):
     return os.path.join(base, name)
 
 kirc = get_path('raw')
-
 counts = {}
 genes = {}
 
@@ -20,9 +17,6 @@ for f in os.listdir(kirc):
         counts[subject] = np.loadtxt(ff, skiprows=1, dtype=np.str, usecols=(1,))
         genes[subject] = np.loadtxt(ff, skiprows=1, dtype=np.str, usecols=(0,))
 
-
-
-
 with open('data.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile)
     subjects = counts.keys()
@@ -30,13 +24,3 @@ with open('data.csv', 'wb') as csvfile:
     data  = np.array(counts.values())
     for gene, count in zip(genes.values()[0], data.T):
         writer.writerow(tuple([gene]+list(count)))
-
-#subject = fname[-26:][:7]
-#counts = np.loadtxt(fname, skiprows=1, dtype=None, usecols=(1,))
-#genes = np.loadtxt(fname, skiprows=1, dtype=np.str, usecols=(0,))
-
-#with open('data.csv', 'wb') as csvfile:
-#    writer = csv.writer(csvfile)
-#    writer.writerow(('gene', subject))
-#    for gene, count in zip(genes, counts):
-#        writer.writerow((gene, str(count)))
